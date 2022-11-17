0c211bb80f32dcda750cc0967f3d55cf5dbb16f3
iamother <xandrewssx970@outlook.es>
2022-11-17 16:19:41 -0500

    Added start cmd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Server on port: ${port}`)
-})
+    console.log("Sever console log.")
+});
