0e04dcda89121925a7fa8d2db0ca1faf9b5cc03d
iamother <xandrewssx970@outlook.es>
2022-11-18 19:54:07 -0500

    added route to app

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.port || 3000;
+const Customers = require('./src/customers/routes');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log("Sever console log.")
+    console.log("Sever on PORT: ", port)
 });
+
+
+app.use('', Customers);
